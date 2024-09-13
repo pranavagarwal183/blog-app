4c0550a6707f38a670a5aa32ec6378f9725b6204
pranavagarwal183 <agarwalpranav177@gmail.com>
2024-09-13 23:50:45 +0530

    Uploading backend

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,81 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {decode,sign,verify} from 'hono/jwt'
+import { signupInput } from '@pranav183/myblogapp-common'
+
+
+export const userRouter=new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+    JWT_SECRET: string
+	}
+}>();
+
+userRouter.post('/signup',async (c) => {
+    const body=await c.req.json();
+    const {success} = signupInput.safeParse(body);
+    if(!success){
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct"
+      })
+    }
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    
+    try {
+      const user=await prisma.user.create({
+        data:{
+          email: body.email,
+          password: body.password,
+        },
+      });
+  
+      if (!c.env.JWT_SECRET) {
+        console.error('JWT_SECRET is not defined');
+        c.status(500);
+        return c.json({ error: 'Server error: JWT secret is missing' });
+      }
+  
+      const token=await sign({id: user.id},c.env.JWT_SECRET)
+      return c.text(token);
+    } catch (error) {
+      c.status(403);
+      return c.json({
+        error: "Error while signing up"
+      });
+    }
+  })
+  
+userRouter.post('/api/v1/signin',async (c) => {
+    const prisma = new PrismaClient({
+          datasourceUrl: c.env?.DATABASE_URL	,
+      }).$extends(withAccelerate());
+  
+      const body = await c.req.json();
+      try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: body.email,
+          password: body.password
+        }
+      });
+    
+      if (!user) {
+        c.status(403);
+        return c.json({ error: "User not found" });
+      }
+    
+      const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+      return c.text(jwt);
+    } catch (error) {
+      console.log(error);
+      c.status(411);
+      return c.text('Invalid');
+    }
+    
+  })
+  
